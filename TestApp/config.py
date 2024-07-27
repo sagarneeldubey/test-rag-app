abb2a93b20d0c6056720a018baf025f45275e523
Sagar Dubey <sdubey@altwallet.ai>
2024-07-27 01:07:22 -0400

    remove api key

--- TestApp/config.py
+++ TestApp/config.py
@@ -1,7 +1,3 @@
 import os
 
-settings = {
-    "OPENAI_API_KEY": os.environ.get(
-        "OPENAI_API_KEY", "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"
-    )
-}
+settings = {"OPENAI_API_KEY": os.environ.get("OPENAI_API_KEY", "your-api-key")}
